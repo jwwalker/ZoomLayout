6313764c42f291fbd9572f95ca3c0ca9994c3840
James Walker <jw@jwwalker.com>
2022-05-28 11:39:12 -0700

    Initial Commit

---
+++ ZoomLayout/AppDelegate.h
@@ -0,0 +1,14 @@
+//  AppDelegate.h
+//  
+//
+//  Created by James Walker on 5/27/22.
+//  
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ZoomLayout/AppDelegate.m
@@ -0,0 +1,33 @@
+//  AppDelegate.m
+//  
+//
+//  Created by James Walker on 5/27/22.
+//  
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+	[self.window makeKeyAndOrderFront: self];
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+	return YES;
+}
+
+
+@end
